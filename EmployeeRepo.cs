da18447dbea90a598fac89e1cfe81df301f749f7
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-16 11:33:12 +0530

    added: connecting to MsSql database

---
+++ EmployeeRepo.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+
+        public void GetAllEmployee()
+        {
+            try
+            {
+                EmployeeModel employeeModel= new EmployeeModel();
+                using(this.connection)
+                {
+                    string query = @"select EmployeeId, EmployeeName, PhoneNumber, Address, Department, Gender, BasicPay, Deductions, TaxablePay, Tax, NetPay, StartDate, City, Country from employee";
+
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
+                    this.connection.Open();
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.HasRows)
+                    {
+                        while(dr.Read())
+                        {
+                            employeeModel.EmployeeId = dr.GetInt32(0);
+                            employeeModel.EmployeeName = dr.GetString(1);
+                            employeeModel.PhoneNumber = dr.GetString(2);
+                            employeeModel.Address = dr.GetString(3);
+                            employeeModel.Department = dr.GetString(4);
+                            employeeModel.Gender = Convert.ToChar(dr.GetString(5));
+                            employeeModel.BasePay = dr.GetDouble(6);
+                            employeeModel.Deductions= dr.GetDouble(7);
+                            employeeModel.TaxablePay = dr.GetDouble(8);
+                            employeeModel.Tax = dr.GetDouble(9);
+                            employeeModel.NetPay = dr.GetDouble(10);
+                            employeeModel.StartDate = dr.GetDateTime(11);
+                            employeeModel.City= dr.GetString(12);
+                            employeeModel.Country= dr.GetString(13);
+
+                            Console.WriteLine(" {0} {1} {2} {3} ", employeeModel.EmployeeId, employeeModel.EmployeeName, employeeModel.PhoneNumber, employeeModel.Country);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Data not found!");
+                    }
+                    dr.Close();
+                    this.connection.Close();
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+
+
+        public bool AddEmployee(EmployeeModel employeeModel)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails", this.connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", employeeModel.EmployeeName);
+                    command.Parameters.AddWithValue("@PhoneNumber", employeeModel.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", employeeModel.Address);
+                    command.Parameters.AddWithValue("@Department", employeeModel.Department);
+                    command.Parameters.AddWithValue("@Gender", employeeModel.Gender);
+                    command.Parameters.AddWithValue("@BasePay", employeeModel.BasePay);
+                    command.Parameters.AddWithValue("@Deductions", employeeModel.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", employeeModel.TaxablePay);
+                    command.Parameters.AddWithValue("@Tax", employeeModel.Tax);
+                    command.Parameters.AddWithValue("@NetPay", employeeModel.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", DateTime.Now);
+                    command.Parameters.AddWithValue("@City", employeeModel.City);
+                    command.Parameters.AddWithValue("@Country", employeeModel.Country);
+                    this.connection.Open();
+
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+}
