506befbf640bddf3c385f90632456f161c20b34d
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-16 11:31:53 +0530

    added: Saving entries to db and retrieving them

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace EmployeePayroll
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll !");
+
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            
+            EmployeeModel employeeModel = new EmployeeModel();
+
+            employeeModel.EmployeeName = "Prince Praveen";
+            employeeModel.PhoneNumber= "45327";
+            employeeModel.Address = "New York";
+            employeeModel.Department = "MVS";
+            employeeModel.Gender = 'M';
+            employeeModel.BasePay = 220000.00;
+            employeeModel.Deductions = 100.00;
+            employeeModel.TaxablePay = 250.00;
+            employeeModel.Tax = 300.00;
+            employeeModel.NetPay = 21000.00;
+            employeeModel.City = "NYC";
+            employeeModel.Country = "USA";
+
+            //employeeRepo.AddEmployee(employeeModel);
+
+            employeeRepo.GetAllEmployee();
+
+
+
+        }
+    }
+}
